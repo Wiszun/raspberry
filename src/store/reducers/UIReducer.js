82428186edf7ff9de6e9926438a0606e7b7979d8
Wiszun <mar.bronowski@gmail.com>
2018-11-18 14:38:11 +0100

    Add files via upload

---
+++ src/store/reducers/UIReducer.js
@@ -0,0 +1,30 @@
+import * as actionTypes from '../actions/actionTypes'
+
+const initState = {
+  showBackdrop: false,
+  showForm: false,
+  hideNav: false,
+  redirect: false
+}
+
+const reducer = (state = initState, action) => {
+  switch (action.type) {
+    case actionTypes.TOGGLE_BACKDROP:
+      return {
+        ...state,
+        showBackdrop: !state.showBackdrop,
+        showForm: action.showForm,
+        hideNav: action.hideNav
+      }
+    case actionTypes.CLOSE_BACKDROP:
+      return {
+        ...state,
+        showBackdrop: false,
+        redirect: true,
+      }
+    default:
+      return state;
+  }
+}
+
+export default reducer;
